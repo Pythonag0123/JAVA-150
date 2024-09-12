bbdbeec3989308938d95062fea0d430450c231ee
Khushi Gangwar <khushigangwar344@gmail.com>
2024-09-12 13:05:17 +0530

    First Commit

---
+++ src/Arrays/Array_Demo.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Array_Demo{
+
+	public static void main(String[] args) {
+		int a;
+		int [] arr=new int[5];
+		System.out.println(arr);
+		int[] other=arr;
+		
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=-9;
+		
+		
+		
+
+	}
+
+}
